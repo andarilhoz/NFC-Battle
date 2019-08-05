ba82c580aef4ff9a2b5d3d291a82cc945566c888
Magno Gouveia <owlflee@gmail.com>
2019-08-05 08:35:42 -0300

    add gps support

--- Assets/_scripts/GameManager.cs
+++ Assets/_scripts/GameManager.cs
@@ -1,5 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -39,12 +43,18 @@ public class GameManager : MonoBehaviour
 
     private void Awake()
     {
-        if ( instance == null ) instance = this;
+        if ( instance == null )
+        {
+            instance = this;
+            Initialize();
+        }
     }
 
-    public void Initialize()
+    public async void Initialize()
     {
-        
+        await EnableFirebase();
+        EnableGooglePlayService();
+        LoginUsingGooglePlay();
     }
 
     public void EditAvatar()
@@ -54,5 +64,82 @@ public class GameManager : MonoBehaviour
     public void AddCreature()
     {
         SceneManager.LoadSceneAsync("AddCreatureScene");
+    }
+
+    private async Task<bool> EnableFirebase()
+    {
+        Debug.Log("Habilitando Firebase");
+        var dependencyStatus  = await Firebase.FirebaseApp.CheckAndFixDependenciesAsync();
+        if ( dependencyStatus == Firebase.DependencyStatus.Available )
+        {
+            // Create and hold a reference to your FirebaseApp,
+            // where app is a Firebase.FirebaseApp property of your application class.
+            //   app = Firebase.FirebaseApp.DefaultInstance;
+
+            // Set a flag here to indicate whether Firebase is ready to use by your app.
+            Debug.Log("Firebase Habilitado");
+            return true;
+        }
+
+        Debug.LogError(System.String.Format(
+                "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+            // Firebase Unity SDK is not safe to use here.
+            return false;
+        
+    }
+
+    private void EnableGooglePlayService()
+    {
+        Debug.Log("Habilitando Google Play");
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder()
+            .RequestServerAuthCode(false /* Don't force refresh */)
+            .RequestEmail()
+            .Build();
+
+        PlayGamesPlatform.InitializeInstance(config);
+        PlayGamesPlatform.Activate();
+        Debug.Log("Google Play Habilitado");
+    }
+
+    private void LoginUsingGooglePlay()
+    {
+        Debug.Log("Realizando login na Google Play");
+        PlayGamesPlatform.Instance.Authenticate((error, message) =>
+        {
+            Debug.Log($"error: {error} message: {message}");
+        });
+        /*Social.localUser.Authenticate((bool success) => {
+            // handle success or failure
+            if ( !success )
+            {
+                Debug.Log("Erro ao fazer o login");
+                return;
+            }
+            Debug.Log("Login realizado com sucesso!");
+            var authCode = PlayGamesPlatform.Instance.GetServerAuthCode();
+            LoginInFirebase(authCode);
+        });*/
+    }
+
+    private void LoginInFirebase(string authCode)
+    {
+        Debug.Log("Realizando Login no Firebase");
+        Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        Firebase.Auth.Credential credential =
+            Firebase.Auth.PlayGamesAuthProvider.GetCredential(authCode);
+        auth.SignInWithCredentialAsync(credential).ContinueWith(task => {
+            if (task.IsCanceled) {
+                Debug.LogError("SignInWithCredentialAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted) {
+                Debug.LogError("SignInWithCredentialAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+        });
     }
 }
